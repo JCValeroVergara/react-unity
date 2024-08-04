dcd7d618e0f8e54fb11dd2d3ace2a265f857b731
Juan Valero <juanvalero@cosmo.edu.co>
2024-08-03 22:28:00 -0500

    chore: Add react-unity-webgl dependency and update package.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
+import {Unity, useUnityContext } from 'react-unity-webgl'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "assets/Build/Build/Build1.loader.js",
+    dataUrl: "assets/Build/Build/Build1.data.gz",
+    frameworkUrl: "assets/Build/Build/Build1.framework.js.gz",
+    codeUrl: "assets/Build/Build/Build1.wasm.gz",
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div
+      className="App"
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Unity
+        style={{
+          width: '90vw',
+          height: '90vh',
+          border: "2px solid black",
+          margin: 'auto',
+          alignItems: 'center',
+          justifyItems: 'center',
+        }}
+        unityProvider={unityProvider}
+      />
+    </div>
   )
 }
 
